6ed9532e7b443897ef251462631cd27be5e79a6f
Gustavo <you@example.com>
2023-11-30 20:23:13 -0300

    Criar branch 'Gustavo'

--- PJI/src/ClienteDao.java
+++ PJI/src/ClienteDao.java
@@ -8,7 +8,7 @@ public class ClienteDao {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/projeto_integrador";
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "2506";
+    private static final String SENHA = "aluno";
 
     public static Connection obterConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
@@ -16,35 +16,33 @@ public class ClienteDao {
 
     public static Cliente buscarCliente(String usuario, String senha) throws SQLException {
         String sql = "SELECT * FROM tabela_clientes WHERE usuario = ? AND senha = ?";
-        
+
         try (Connection connection = obterConexao();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-    
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
             preparedStatement.setString(1, usuario);
             preparedStatement.setString(2, senha);
-    
+
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
                     return new Cliente(
-                        resultSet.getString("cnpj"),
-                        resultSet.getString("email"),
-                        resultSet.getString("nome_completo"),
-                        resultSet.getString("senha"),
-                        resultSet.getString("usuario")
-                    );
+                            resultSet.getString("cnpj"),
+                            resultSet.getString("email"),
+                            resultSet.getString("nome_completo"),
+                            resultSet.getString("senha"),
+                            resultSet.getString("usuario"));
                 } else {
-                    return null; 
+                    return null;
                 }
             }
         }
     }
-    
 
     public boolean salvarCliente(Cliente cliente) {
         String sql = "INSERT INTO tabela_clientes (cnpj, email, nome_completo, senha, usuario) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conexao = obterConexao();
-             PreparedStatement statement = conexao.prepareStatement(sql)) {
+                PreparedStatement statement = conexao.prepareStatement(sql)) {
 
             statement.setString(1, cliente.getCnpj());
             statement.setString(2, cliente.getEmail());
@@ -66,7 +64,7 @@ public class ClienteDao {
         String sql = "SELECT COUNT(*) FROM tabela_clientes WHERE usuario = ?";
 
         try (Connection conexao = obterConexao();
-             PreparedStatement statement = conexao.prepareStatement(sql)) {
+                PreparedStatement statement = conexao.prepareStatement(sql)) {
 
             statement.setString(1, usuario);
 
