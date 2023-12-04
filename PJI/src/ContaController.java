a88b11556845904199acb18b79aa092749793a65
BiaTrixie <bmoura276@gmail.com>
2023-12-04 13:13:31 -0300

    correção telas

--- PJI/src/ContaController.java
+++ PJI/src/ContaController.java
@@ -1,13 +1,7 @@
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 public class ContaController {
 
@@ -64,7 +58,7 @@ public class ContaController {
 
     @FXML
     void telaPainel(ActionEvent event) {
-        abrirTelaPainel(event);
+
     }
 
     @FXML
@@ -72,35 +66,4 @@ public class ContaController {
 
     }
 
-    @FXML
-    private Cliente clienteLogado;
-
-    private Cliente cliente;  
-
-      private void abrirTelaPainel(ActionEvent event) {
-             try {
-                FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("seuArquivoFXML.fxml"));
-                Parent root = fxmloader.load();
-                Scene cena = new Scene(root);
-                
-                Stage stage = (Stage) btnProd.getScene().getWindow();
-                stage.close();
-                
-                Stage novaJanela = new Stage();
-                novaJanela.setTitle("Painel");
-                novaJanela.setScene(cena);
-                novaJanela.show();
-                
-                // Agora chame setClienteLogado
-                ContaController contaController = fxmloader.getController();
-                contaController.setClienteLogado(cliente);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void setClienteLogado(Cliente cliente) {
-        this.clienteLogado = cliente;
-        lbnome.setText(cliente.getUsuario());
-    }
 }
