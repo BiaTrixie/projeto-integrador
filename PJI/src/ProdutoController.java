f67c5b1e4746a2783feb409a70515989d37a4af3
BiaTrixie <bmoura276@gmail.com>
2023-12-12 18:43:38 -0300

    att

--- PJI/src/ProdutoController.java
+++ PJI/src/ProdutoController.java
@@ -1,11 +1,11 @@
 
 import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -14,19 +14,10 @@ import javafx.stage.Stage;
 public class ProdutoController {
 
     @FXML
-    private Button btnConfig;
-
-    @FXML
-    private Button btnConta;
-
-    @FXML
-    private Button btnEnviar;
-
-    @FXML
-    private Button btnFaturamento;
-
-    @FXML
     private Button btnPainel;
+
+    @FXML
+    private Label lbnome;
 
     @FXML
     private Label lbCadastroProd;
@@ -42,12 +33,6 @@ public class ProdutoController {
 
     @FXML
     private Label lbPainel;
-
-    @FXML
-    private Label lbemail;
-
-    @FXML
-    private Label lbnome;
 
     @FXML
     private TextField txCodigo;
@@ -71,117 +56,19 @@ public class ProdutoController {
     private TextField txValorRev;
 
     @FXML
-    void saveProduto(ActionEvent event) {
-        Produto produto = new Produto();
-        produto.setCodigo(txCodigo.getText());
-        produto.setDescricao(txDescricao.getText());
-        produto.setNome(txNomeProd.getText());
-
-        if (!txValorProd.getText().isEmpty()) {
-            String valorProdText = txValorProd.getText().replace(",", ".");
-            produto.setValorProduto(Double.parseDouble(valorProdText));
-        } else {
-            exibirAlerta("alerta", "Preencha todos os campos!");
-            return;
-        }
-
-        if (!txValorRev.getText().isEmpty()) {
-            String valorRevText = txValorRev.getText().replace(",", ".");
-            produto.setValorRevenda(Double.parseDouble(valorRevText));
-        } else {
-            exibirAlerta("alerta", "Preencha todos os campos!");
-            return;
-        }
-        
-        ProdutoDAO produtoDAO = new ProdutoDAO();
-        produtoDAO.salvarProduto(produto);
-        exibirAlerta("Aviso", "Produto salvo no banco de dados!");
-    }
+    private Cliente clienteLogado;  
 
     @FXML
     void telaPainel(ActionEvent event) {
-        abrirTelaPainel(event);
+         abrirTelaPainel(event);
     }
 
     @FXML
-    void telaConfig(ActionEvent event) {
-        abrirTelaConfig(event);
+    void telaproduto(ActionEvent event) {
+
     }
 
-    @FXML
-    void telaConta(ActionEvent event) {
-        abrirTelaConta(event);
-    }
-
-    @FXML
-    void telaFaturamento(ActionEvent event) {
-        abrirTelafaturamento(event);
-    }
-
-    @FXML
-    private Cliente clienteLogado;
-
-    private ClienteDao clienteDao;
-
-    public ProdutoController() {
-        this.clienteDao = new ClienteDao();
-    }
-
-    private void abrirTelaConfig(ActionEvent event) {
-        try {
-            FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("config.fxml"));
-            Parent root = fxmloader.load();
-            Scene telaInicial = new Scene(root);
-
-            Stage stage = (Stage) btnPainel.getScene().getWindow();
-            stage.close();
-
-            Stage novaJanela = new Stage();
-            novaJanela.setTitle("Configurações");
-            novaJanela.setScene(telaInicial);
-            novaJanela.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void abrirTelaConta(ActionEvent event) {
-        try {
-            FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("conta.fxml"));
-            Parent root = fxmloader.load();
-            Scene telaInicial = new Scene(root);
-
-            Stage stage = (Stage) btnPainel.getScene().getWindow();
-            stage.close();
-
-            Stage novaJanela = new Stage();
-            novaJanela.setTitle("Conta");
-            novaJanela.setScene(telaInicial);
-            novaJanela.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void abrirTelafaturamento(ActionEvent event) {
-        try {
-            FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("faturamento.fxml"));
-            Parent root = fxmloader.load();
-            Scene telaInicial = new Scene(root);
-
-            Stage stage = (Stage) btnPainel.getScene().getWindow();
-            stage.close();
-
-            Stage novaJanela = new Stage();
-            novaJanela.setTitle("Tela de Faturamento");
-            novaJanela.setScene(telaInicial);
-            novaJanela.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void abrirTelaPainel(ActionEvent event) {
+       private void abrirTelaPainel(ActionEvent event) {
         try {
             FXMLLoader fxmloader = new FXMLLoader(getClass().getResource("painel.fxml"));
             Parent root = fxmloader.load();
@@ -191,7 +78,7 @@ public class ProdutoController {
             stage.close();
 
             Stage novaJanela = new Stage();
-            novaJanela.setTitle("Painel");
+            novaJanela.setTitle("Tela de Login");
             novaJanela.setScene(telaInicial);
             novaJanela.show();
         } catch (IOException e) {
@@ -199,25 +86,9 @@ public class ProdutoController {
         }
     }
 
-    private void exibirAlerta(String titulo, String mensagem) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(titulo);
-        alert.setHeaderText(null);
-        alert.setContentText(mensagem);
-        alert.showAndWait();
-    }
-
-    @FXML
-    void initialize() {
-        String usuario = ApplicationContext.getUsuarioLogado();
-
-        if (usuario != null) {
-            lbnome.setText(usuario);
-
-            // Agora pegamos o e-mail diretamente do ApplicationContext
-            String email = ApplicationContext.getEmailLogado();
-            lbemail.setText(email);
-        }
+    public void setClienteLogado(Cliente cliente) {
+        this.clienteLogado = cliente;
+        lbnome.setText(cliente.getUsuario());
     }
 
 }
